8106fbcee82747dfd86553792fe29be55e049706
Hiroki73LS <hayashi_htt@yahoo.co.jp>
2020-12-08 22:43:55 +0900

    New1208

---
+++ Realm9/ContentView.swift
@@ -0,0 +1,149 @@
+import SwiftUI
+import RealmSwift
+
+struct NavigationConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void = { _ in }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let nc = uiViewController.navigationController {
+            self.configure(nc)
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var model = viewModel()
+    @State private var idDetail = ""
+    @State private var taskDetail = ""
+    @State private var task2Detail = ""
+    @State private var task3Detail = ""
+    @State private var dateDetail = Date()
+    @State private var isONDetail = false
+    @State private var pick1Detail = 0
+    @State private var toSave = false
+    @State private var alert = false
+    @State private var alert1 = false
+    @State private var isShown: Bool = false
+    @State private var showingAlert = false
+    @State private var showAlert = false
+    
+    var dateFormat: DateFormatter {
+        let dformat = DateFormatter()
+        dformat.dateFormat = "yyyy/M/d h:mm"
+        return dformat
+    }
+    
+    let backGroundColor = LinearGradient(gradient: Gradient(colors: [Color.white, Color.green]), startPoint: .top, endPoint: .bottom)
+    
+    init() {
+            UITableView.appearance().backgroundColor = .clear
+            UITableViewCell.appearance().backgroundColor = .clear
+        }
+    
+    var body: some View {
+        NavigationView {
+        ZStack{
+            backGroundColor.edgesIgnoringSafeArea(.all)
+            VStack{
+                
+                    List{
+                        ForEach(model.myModels.sorted{$0.date > $1.date}, id: \.id) {
+                                myModel in
+
+                            Button(action: {
+                                idDetail = myModel.id
+                                taskDetail = myModel.task
+                                task2Detail = myModel.task2
+                                task3Detail = myModel.task3
+                                isONDetail = myModel.isON
+                                dateDetail = myModel.date
+                                self.showAlert = true
+                                            }, label: {
+                                                
+                            NavigationLink(destination: EditView(task: $taskDetail, task2: $task2Detail, task3: $task3Detail, date: $dateDetail, isON: $isONDetail, pick1: $pick1Detail), isActive: $showAlert) {
+                                    HStack{
+                                        VStack(alignment:.leading) {
+                                                Text(myModel.task)
+                                                .font(.title)
+                                                Spacer()
+                                            VStack{
+                                                HStack{
+                                                    Text(myModel.task2)
+                                                        .foregroundColor(Color.gray)
+                                                    Spacer()
+                                                }
+                                                HStack{
+                                                    Text(myModel.task3)
+                                                    .foregroundColor(Color.gray)
+                                                    Spacer()
+                                                    Text(dateFormat.string(from: myModel.date))
+                                                }
+                                            }
+                                            .padding(0.0)
+                                        }
+                                        if myModel.isON == true {
+                                            Image(systemName: "heart.circle.fill")
+                                                .foregroundColor(.pink)
+                                                .onTapGesture {
+                                                    try? Realm().write {
+                                                        myModel.isON = false
+                                                    }}
+                                        } else {
+                                            Image(systemName: "heart.circle.fill")
+                                                .foregroundColor(.secondary)
+                                                .onTapGesture {
+                                                    try? Realm().write {
+                                                        myModel.isON = true
+                                                    }}
+                                        }
+                                    }
+                            }.listRowBackground(Color.clear)
+                            }).background(Color.clear)
+                        }
+                        .onDelete { indexSet in
+                        if let index = indexSet.first {
+                           let realm = try? Realm()
+                           try? realm?.write {
+                           realm?.delete(self.model.myModels[index])
+                                                }
+                                            }
+                        }
+                    }
+            }
+            .background(NavigationConfigurator { nc in
+             nc.navigationBar.barTintColor = #colorLiteral(red: 0.9033463001, green: 0.9756388068, blue: 0.9194290638, alpha: 1)
+             nc.navigationBar.titleTextAttributes = [.foregroundColor : UIColor.white]
+            })
+            }
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarItems(leading:
+          
+            Button(action: {
+                let realm = try! Realm()
+                try! realm.write {
+                    realm.deleteAll()
+                }})
+           {
+           Image(systemName: "gearshape")
+           },
+            trailing:
+            HStack {
+                Button(action: {
+                    self.isShown = true
+                }) {
+                    Image(systemName: "square.and.pencil")
+                } .sheet(isPresented: self.$isShown) {
+                    //モーダル遷移した後に表示するビュー
+                    EnterView()
+                }
+            })
+        }}}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Realm9/EnterView.swift
@@ -0,0 +1,148 @@
+import SwiftUI
+import RealmSwift
+
+class model: Object {
+    @objc dynamic var id = UUID().uuidString
+    @objc dynamic var task = ""
+    @objc dynamic var task2 = ""
+    @objc dynamic var task3 = ""
+    @objc dynamic var isON = false
+    @objc dynamic var date = Date()
+}
+
+class viewModel: ObservableObject {
+    private var token: NotificationToken?
+    private var myModelResults = try? Realm().objects(model.self)
+    @Published var myModels: [model] = []
+
+    init() {
+        token = myModelResults?.observe { [weak self] _ in
+            self?.myModels = self?.myModelResults?.map { $0 } ?? []
+        }
+    }
+
+    deinit {
+        token?.invalidate()
+    }
+}
+    
+struct EnterView: View {
+    
+    @ObservedObject var model = viewModel()
+    @State private var task = ""
+    @State private var task2 = ""
+    @State private var task3 = ""
+    @State private var date = Date()
+    @State private var isON = false
+    @State private var pick1 = 0
+    @State private var toSave = false
+    @State private var alert = false
+    @State private var alert1 = false
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var sentakusi = ["練習・課題","出欠席","その他"]
+
+    var dateFormat: DateFormatter {
+        let dformat = DateFormatter()
+        dformat.dateFormat = "yyyy/M/d h:mm"
+        return dformat
+    }
+    
+    let backGroundColor = LinearGradient(gradient: Gradient(colors: [Color.white, Color.green]), startPoint: .top, endPoint: .bottom)
+    
+    init() {
+            UITableView.appearance().backgroundColor = .clear
+            UITableViewCell.appearance().backgroundColor = .clear
+        }
+    
+    var body: some View {
+
+        NavigationView {
+            ZStack{
+            backGroundColor.edgesIgnoringSafeArea(.all)
+            Form {
+                Section(header: Text("Title & Doの入力")) {
+                    TextField("[名前,タイトル]を入力してください", text: $task)
+                    TextField("[内容,Do]を入力してください", text: $task2)
+                    DatePicker(selection: $date, displayedComponents: .date,label: {Text("登録日時")} )
+                    HStack {
+                        Text("お気に入り")
+                        Toggle(isOn: $isON) {
+                        EmptyView()
+                        }
+                    }
+                }
+                Section(header: Text("カスタムタイプを選択")) {
+                    
+                    //-Picker--------------------------
+                    Picker(selection: $pick1,
+                           label: Text("")) {
+                        Text("練習・課題").tag(0)
+                        Text("出欠席").tag(1)
+                        Text("その他").tag(2)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    //-Picker--------------------------
+                    
+                }
+                Section{
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            if self.task == "" {
+                                self.alert = false
+                                self.alert1.toggle()
+                            }else{
+                                self.alert1.toggle()
+                                self.toSave = true
+                //-書き込み--------------------------
+                                let models = Realm9.model()
+                                models.task = task
+                                models.task2 = task2
+                                models.task3 = sentakusi[pick1]
+                                models.isON = isON
+                                models.date = date
+                                let realm = try? Realm()
+                                try? realm?.write {
+                                    realm?.add(models)
+                                }
+                //-書き込み--------------------------
+                                self.alert = true
+                                }
+                        }){
+                    Text("確定")
+                        }
+                        .padding()
+                        .alert(isPresented: $alert1) {
+                            switch(alert) {
+                                case false:
+                                 return
+                                    Alert(title: Text("注意"),
+                                     message: Text("[名前,タイトル]を入力してください"),
+                                     dismissButton: .default(Text("OK")))
+                                case true:
+                                 return
+                                    Alert(title: Text("確認"),
+                                          message: Text("\(task)さんの内容を登録しました。"),
+                                    dismissButton: .default(Text("OK"),
+                                    action: {
+                                        task = ""
+                                        task2 = ""
+                                        task3 = ""
+                                        isON = false
+                                        self.presentationMode.wrappedValue.dismiss()
+                                    }))
+                             }
+                        }
+                    Spacer()
+            }
+        }
+            }.navigationBarTitle("Enter The Info")
+            }}
+    }
+    }
+
+struct EnterView_Previews: PreviewProvider {
+    static var previews: some View {
+        EnterView()
+    }
+}
